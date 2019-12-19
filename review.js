2f8d7074d4c31882e29d66a9fe04642537f3067d
Timothy Shores <timothymshores@gmail.com>
2019-12-19 01:16:11 -0600

    Update review.js

--- review.js
+++ review.js
@@ -1,8 +1,18 @@
-const name = "Tim";
-let hasHobbies = true;
-let age = 30;
+// const name = "Tim";
+// let hasHobbies = true;
+// let age = 30;
 
 const summarizeUser = (userName, userAge, userHasHobby) =>
 	`Name is ${userName}, age is ${userAge} and the user has hobbies: ${userHasHobby}`;
 
-console.log(summarizeUser(name, age, hasHobbies));
+const person = {
+	name: "Tim",
+	age: 30,
+	greet() {
+		console.log(`Hi, I am ${this.name}`);
+	}
+};
+
+// console.log(summarizeUser(name, age, hasHobbies));
+
+person.greet();
