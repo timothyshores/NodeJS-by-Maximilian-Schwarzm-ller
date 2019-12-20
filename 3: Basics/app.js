3ef265f0032cfd01bdc1220d15dfbee0428d1a26
Timothy Shores <timothymshores@gmail.com>
2019-12-20 05:35:48 -0600

    Create routes.js and import into app.js

--- 3: Basics/app.js
+++ 3: Basics/app.js
@@ -1,36 +1,8 @@
-// import http & fs core modules included with Node.js
 const http = require("http");
-const fs = require("fs");
+const routes = require("./routes");
 
 // create const server and set equal to the console log CB function
-const server = http.createServer((req, res) => {
-	const { url, method } = req;
-	if (url === "/") {
-		res.write(
-			'<html><head><title>Enter Message</title></head><body><h1><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></h1></body></html>'
-		);
-		return res.end();
-	}
-
-	if (url === "/message" && method === "POST") {
-		const body = [];
-		req.on("data", chunk => {
-			console.log("chunk", chunk);
-			body.push(chunk);
-		});
-		req.on("end", () => {
-			const parsedBody = Buffer.concat(body).toString();
-			const message = parsedBody.split("=")[1];
-			console.log("message", message);
-			fs.writeFileSync("message.txt", message);
-		});
-		res.statusCode = 302;
-		res.setHeader("Location", "/");
-		return res.end();
-	}
-
-	res.setHeader("Content-Type", "text/html");
-});
+const server = http.createServer(routes);
 
 // Creates server on localhost:3000
 server.listen(3000);
